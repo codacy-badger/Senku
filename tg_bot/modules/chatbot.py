114ce7dfed5991decd463e1c8e3dcbe31460c3a6
Aftab Chouhan <62333331+FtSasaki@users.noreply.github.com>
2020-04-21 23:32:04 +0530

    Senkuu_bot

--- tg_bot/modules/chatbot.py
+++ tg_bot/modules/chatbot.py
@@ -90,13 +90,13 @@ def chatbot(bot: Bot, update: Update):
 __mod_name__ = "Chatbot"
 
 __help__ = """
-Chatbot utilizes the CoffeeHouse API and allows Senkuu_bot to talk back making your chat more interactive.
+Chatbot utilizes the CoffeeHouse API and allows Senku to talk back making your chat more interactive.
 This is an ongoing upgrade and is only available in your chats if you reach out to @Sciencers and ask for it. 
 
 In future we might make it open for any chat and controllable by group admins.
 
 Powered by CoffeeHouse (https://coffeehouse.intellivoid.net/) from @Intellivoid
-Commands: These only work for Senkuu Staff users. 
+Commands: These only work for Senku Staff users. 
  - /addchat : Enables Chatbot mode in the chat.
  - /rmchat  : Disables Chatbot mode in the chat.
 """
